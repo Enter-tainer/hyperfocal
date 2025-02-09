2dee2ae818af592d12f9d2b5f63145e6b9f7bbc1
Enter-tainer <Enter-tainer@users.noreply.github.com>
2025-02-09 15:15:58 +0000

    deploy: 8b8773d1c6ad6f7dd0265b3cfef286533c0d6524

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'hyperfocal-calculator-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon-192.png',
+  './icon-512.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
